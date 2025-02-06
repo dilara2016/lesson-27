9ba162d5ec5ec0ce7d04a523f032cb9246e4e67a
dilara2016 <m.123.hunain@gmail.com>
2025-02-06 20:18:22 +0100

    Update inheritence .py

--- inheritence .py
+++ inheritence .py
@@ -6,6 +6,6 @@ class vehicle:
 class bus (vehicle):
     pass
 
-school_bus=bus("school volvo", 180, 12)
+school_bus=bus("school volvo", 100, 122)
 print("vehicle name:",school_bus.name, "speed:",school_bus.max_speed,
 "mileage:", school_bus.mileage)
